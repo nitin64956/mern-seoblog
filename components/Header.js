39db6169203209b8b06567609b066b8d3eaeafc4
nitin64956 <nitin64956@gmail.com>
2020-11-14 11:39:23 +0530

    Seoblog first commit to deploy

---
+++ components/Header.js
@@ -0,0 +1,107 @@
+import { useState,useEffect } from 'react';
+import {APP_NAME} from '../config'
+
+import {signout,isAuth} from '../actions/auth'
+
+import NProgress from 'nprogress'
+
+
+import Link from 'next/link';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+
+//import '../node_modules/nprogress/nprogress.css'
+
+import Router from 'next/router';
+
+import Search from '../components/blog/Search' 
+
+Router.onRouteChangeStart = url => NProgress.start()
+Router.onRouteChangeComplete = url => NProgress.done()
+
+Router.onRouteChangeError = url => NProgress.done()
+
+
+const Header = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <React.Fragment>
+          <Navbar color="light" light expand="md">
+            <Link href="/">
+            <NavLink className="font-weight-bold" >{APP_NAME}</NavLink>
+            </Link>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <React.Fragment>
+                  <NavItem>
+                    <Link href="/blogs">
+                      <NavLink>Blogs</NavLink>
+                    
+                    </Link>
+
+                  </NavItem>
+
+                  <NavItem>
+                    <Link href="/contact">
+                      <NavLink>Contact</NavLink>
+                    </Link>
+                  </NavItem>
+                </React.Fragment>
+
+               {!isAuth() && <React.Fragment>
+                <NavItem>
+                  <Link href="/signin"><NavLink>Signin</NavLink></Link>
+                </NavItem>
+
+                <NavItem>
+                  <Link href="/signup"><NavLink>Signup</NavLink></Link>
+                </NavItem>
+               </React.Fragment> }
+               
+                {isAuth() && isAuth().role === 0 && (<NavItem>
+                  <Link href="/user">
+                    <NavLink > {`${isAuth().name}`}'s Dashboard</NavLink>
+                  </Link>
+                
+                </NavItem>)}
+
+
+                {isAuth() && isAuth().role === 1 && (<NavItem>
+                  <Link href="/admin">
+                    <NavLink > {`${isAuth().name}`}'s Dashboard</NavLink>
+                  </Link>
+                
+                </NavItem>)}
+
+                {isAuth() &&   (<NavItem>
+                    <NavLink style={{cursor:"pointer"}} onClick={() => signout(() => Router.push('/signin'))} > Signout </NavLink>
+                </NavItem>)}
+
+                <NavItem>
+                  <Link href="/user/crud/blog">
+                    <NavLink className="btn btn-primary text-light">Write a blog</NavLink>
+                  </Link>
+                </NavItem>
+
+              </Nav>
+            </Collapse>
+          </Navbar>
+          <Search/>
+        </React.Fragment>
+      );
+
+}
+
+
+export default Header;
---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+              <meta charSet="UTF-8"/>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossOrigin="anonymous" />
+              {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" /> */}
+              <link rel="stylesheet" href="/static/css/styles.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
